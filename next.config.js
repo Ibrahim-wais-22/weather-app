0de59678f39cf2152337d600304097802f1f7121
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-10-10 19:29:58 -0700

    edit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["images.unsplash.com", "assets.weatherstack.com"],
+    domains: ["assets.weatherstack.com"],
 },
 }
 
